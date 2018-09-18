c929150821051d9606a59cced21ccbfa758b75dd
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-09-18 13:44:22 +0300

    dxfghjkiuytr

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function triangletracker() {
+var AB =document.getElementById('AB').value;
+var BC =document.getElementById('BC').value;
+var CA =document.getElementById('CA').value;
+
+
+     if (AB ===BC && BC===CA || CA === AB){
+
+     alert("Equilateral triangle!");
+ }
+ else if (BC===AB && BC === CA || CA === AB && CA != BC) {
+    alert("Isosceles triangle!");
+ }
+ else if (AB != BC && AB !=CA || BC !=CA ) {
+     alert("Scalene triangle!");
+ }
+ else if( AB + BC <= CA || CA + BC <= AB){
+   alert("Not a triangle!");
+ }
+}
