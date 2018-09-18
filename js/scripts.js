982fda7673c5683b6bc843583b5889951def0b72
Francis Sakwa <42961784+FrancisSakwa89@users.noreply.github.com>
2018-09-18 14:57:39 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,18 @@ var AB =document.getElementById('AB').value;
 var BC =document.getElementById('BC').value;
 var CA =document.getElementById('CA').value;
 
-  if (AB ===BC && BC===CA || CA === AB){
+
+if (AB ===BC && BC===CA || CA === AB){
 
 alert("Equilateral triangle!");
 }
 else if (BC===AB && BC === CA || CA === AB && CA != BC) {
 alert("Isosceles triangle!");
 }
-else if (AB > BC && AB > CA || BC > CA) {
+else if (AB != BC && AB !=CA || BC !=CA ) {
 alert("Scalene triangle!");
 }
 else if( AB + BC <= CA || CA + BC <= AB){
 alert("Not a triangle!");
-}
- else{
-      alert("please be keen and try again")
 }
 }
