da7740d4d3c038e61c23bd7eb4d563706ea01d97
Francis Sakwa <42961784+FrancisSakwa89@users.noreply.github.com>
2018-09-18 14:48:49 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var AB =document.getElementById('AB').value;
 var BC =document.getElementById('BC').value;
 var CA =document.getElementById('CA').value;
 
-  if ((AB < BC)  &&(CA < BC) || (BC < AB) {
+  if (AB < 0 && BC < 0 || CA < 0) {
        alert ("No triangle")
 }
  else if (AB ===BC && BC===CA || CA === AB){
